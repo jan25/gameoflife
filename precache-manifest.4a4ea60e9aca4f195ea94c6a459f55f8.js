aaa6d5e1d68b41476affbd3d8140c0a522e24c6a
Abhilash Gnan <abhilashgnan@gmail.com>
2020-04-19 20:21:24 +0200

    Updates

---
+++ precache-manifest.4a4ea60e9aca4f195ea94c6a459f55f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f543ef0938f4fa486d8b52b2e3fc485",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "5084b2bd18e9f69fb355",
+    "url": "/gameoflife/static/css/main.158f2594.chunk.css"
+  },
+  {
+    "revision": "bddcb2881c1037b91c4f",
+    "url": "/gameoflife/static/js/2.4e9a39b9.chunk.js"
+  },
+  {
+    "revision": "75fce69b6dea873e4a3f62ef0bb2c96f",
+    "url": "/gameoflife/static/js/2.4e9a39b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5084b2bd18e9f69fb355",
+    "url": "/gameoflife/static/js/main.a6285b3a.chunk.js"
+  },
+  {
+    "revision": "eefcc6c47d48c3bdf661",
+    "url": "/gameoflife/static/js/runtime-main.9461848e.js"
+  }
+]);
